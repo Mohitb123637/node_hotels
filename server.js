f087e09cd3d8a1421b2d2679a979d5bebc2c9c63
mohit <mohitb12363@gamil.com>
2024-09-01 13:18:44 +0530

    add something

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ const menuRoutes = require("./routes/menuRoutes.js");
 app.use('/person', personRoutes);
 app.use('/menu', menuRoutes);
 
+app.use("/",(req, res) => {
+  res.send("Welcome to hotel");
+ });
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log('Server is running on port 3000');
