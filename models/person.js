a70bb2d2b725ebbd13de68870d4d28cd3fc0f4e6
mohit <mohitb12363@gamil.com>
2024-09-03 13:11:03 +0530

    add authentication and authorization

--- models/person.js
+++ models/person.js
@@ -1,20 +1,51 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const personSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    age: { type: Number, required: true },
-    work: { type: String, enum: ['chef', 'waiter', 'manager'], required: true },
-    email: { type: String, required: true, unique: true },
-    mobile: { type: String, required: true, unique: true },
-    address:{
-        type: String,
-        required: true,
-    
-    },
-    salary:{
-        type: Number,
-        required: true,
-    }
+  name: { type: String, required: true },
+  age: { type: Number, required: true },
+  work: { type: String, enum: ['chef', 'waiter', 'manager'], required: true },
+  email: { type: String, required: true, unique: true },
+  mobile: { type: String, required: true, unique: true },
+  address: {
+    type: String,
+    required: true,
+  },
+  salary: {
+    type: Number,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: { type: String, required: true },
+});
+
+personSchema.methods.comparePassword = async function (candidatePassword) {
+  try {
+    const isMatch = await bcrypt.compare(candidatePassword, this.password);
+    return isMatch;
+  } catch (error) {
+    next(error);
+  }
+};
+
+personSchema.pre('save', async function (next) {
+  const person = this;
+
+  //   Hash the password only if it has been modified (or is new)
+  if (!person.isModified('password')) return next();
+  try {
+    // hash password generate
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(person.password, salt);
+    person.password = hashedPassword;
+    next();
+  } catch (error) {
+    return next(error);
+  }
 });
 
 const Person = mongoose.model('Person', personSchema);
--- server.js
+++ server.js
@@ -6,16 +6,35 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 const personRoutes = require("./routes/personRoutes.js");
 const menuRoutes = require("./routes/menuRoutes.js");
+const passport = require('./auth.js');
 
-app.use('/person', personRoutes);
-app.use('/menu', menuRoutes);
+// Middleware for parsing JSON bodies
+app.use(bodyParser.json());
+// Custom middleware to log requests
+const logRequest = (req, res, next) => {
+  console.log(`${new Date().toLocaleString()} Request Made to: ${req.originalUrl}`);
+  next();
+};
 
-app.use("/",(req, res) => {
+// Log requests
+app.use(logRequest);
+
+// Initialize Passport.js
+app.use(passport.initialize());
+const localAuthMiddleware = passport.authenticate('local', { session: false });
+
+
+// Public route
+app.get("/",(req, res) => {
   res.send("Welcome to hotel");
- });
+});
 
+// Routes requiring authentication
+app.use('/person', personRoutes);
+app.use('/menu', localAuthMiddleware, menuRoutes);
 
-const PORT = process.env.PORT || 5000;
+// Start the server
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('Server is running on port 3000');
 });
